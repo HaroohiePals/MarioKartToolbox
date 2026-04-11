bb5e43363623f5929718360d91d674bd5b860ad3
Ermelber <9807588+Ermelber@users.noreply.github.com>
2026-04-11 18:33:43 +0200

    Add Global Map editing (wip)

--- src/HaroohiePals.MarioKartToolbox/OpenGL/RenderGroups/Minimap/GlobalMapRenderGroup.cs
+++ src/HaroohiePals.MarioKartToolbox/OpenGL/RenderGroups/Minimap/GlobalMapRenderGroup.cs
@@ -1,9 +1,11 @@
 #nullable enable
+using System;
 using HaroohiePals.Graphics3d;
 using HaroohiePals.Graphics3d.OpenGL;
 using HaroohiePals.Graphics3d.OpenGL.Renderers;
 using HaroohiePals.Gui.Viewport;
 using HaroohiePals.MarioKartToolbox.OpenGL.Renderers;
+using HaroohiePals.Mathematics;
 using HaroohiePals.Nitro.Gx;
 using HaroohiePals.Nitro.NitroSystem.G2d;
 using HaroohiePals.NitroKart.Course;
@@ -13,9 +15,13 @@ namespace HaroohiePals.MarioKartToolbox.OpenGL.RenderGroups.Minimap;
 
 class GlobalMapRenderGroup : RenderGroup
 {
+    private const float QUAD_HEIGHT_Y = 3000f;
+
     private readonly QuadRenderer? _quadRenderer;
     private readonly QuadRenderer? _quadRendererTranslucent;
     private readonly IMkdsCourse _course;
+
+    private MkdsGlobalMapSettings _mapSettings => _course.Metadata.GlobalMapSettings!;
 
     public bool RenderTranslucent { get; set; } = false;
 
@@ -37,11 +43,86 @@ class GlobalMapRenderGroup : RenderGroup
         if (renderer is null)
             return;
 
-        var color = Color4.White;
-        renderer.Points = [new InstancedPoint(new Vector3(0, 3000, 300), Vector3.Zero,
-            new Vector3(100), color, true, null, ViewportContext.InvalidPickingId,
-            false, false)];
+        var transform = GetCurrentTransform();
+        var position = (Vector3)transform.Translation;
+        var scale = (Vector3)transform.Scale / 10;
+        var rotation = _mapSettings.Rotate90Degrees ? new Vector3(0, 90, 0) : Vector3.Zero;
+
+        uint pickingId = context.GetPickingId(PickingGroupId, 0);
+        bool isHovered = context.IsHovered(_mapSettings);
+
+        renderer.Points = [new InstancedPoint(position, rotation, scale, Color4.White, true, _mapSettings,
+            pickingId, isHovered, false)];
         renderer.Render(context);
+    }
+
+    public override object GetObject(int index) => _mapSettings;
+
+    public override bool ContainsObject(object obj) => obj == _mapSettings;
+
+    public override bool TryGetObjectTransform(object obj, int subIndex, out Transform transform)
+    {
+        if (obj != _mapSettings)
+        {
+            transform = Transform.Identity;
+            return false;
+        }
+
+        transform = GetCurrentTransform();
+        return true;
+    }
+
+    public override bool TrySetObjectTransform(object obj, int subIndex, in Transform transform)
+    {
+        if (obj != _mapSettings)
+            return false;
+
+        double halfTotalWidth = transform.Scale.X;
+        double halfHeight = transform.Scale.Z;
+        double centerX = transform.Translation.X;
+        double centerY = transform.Translation.Z;
+
+        double totalWidth = halfTotalWidth * 2f;
+        double height = halfHeight * 2f;
+        double leftEdge = centerX - totalWidth * 0.5;
+        double topEdge = centerY - height * 0.5;
+
+        _mapSettings.TopLeft = new Vector2d(Math.Round(leftEdge), Math.Round(topEdge));
+        _mapSettings.BottomRight = new Vector2d(Math.Round(leftEdge + totalWidth), Math.Round(topEdge + height));
+
+        return true;
+    }
+
+    public override bool TryGetLocalObjectBounds(object obj, int subIndex, out Box3d bounds)
+    {
+        if (obj != _mapSettings)
+        {
+            bounds = new Box3d();
+            return false;
+        }
+
+        var scale = GetCurrentTransform().Scale;
+        bounds = new Box3d(
+            new Vector3d(-scale.X, 0, -scale.Z),
+            new Vector3d(scale.X, 0, scale.Z));
+        return true;
+    }
+
+    private Transform GetCurrentTransform()
+    {
+        var topLeft = _mapSettings.TopLeft;
+        var bottomRight = _mapSettings.BottomRight;
+
+        double width = bottomRight.X - topLeft.X;
+        double height = bottomRight.Y - topLeft.Y;
+
+        double centerX = topLeft.X + width * 0.5;
+        double centerY = (topLeft.Y + bottomRight.Y) * 0.5;
+
+        return new Transform(
+            new Vector3d(centerX, QUAD_HEIGHT_Y, centerY),
+            Vector3d.Zero,
+            new Vector3d(width * 0.5f, 1, height * 0.5f));
     }
 
     private GLTexture? CreateTexture(bool translucent)
